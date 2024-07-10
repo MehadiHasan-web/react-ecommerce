a946a129d5768a92d740509fa1ee8845f4b48d66
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-07-10 16:27:37 +0600

    install daisyUI

--- src/Page/SinglePage.jsx
+++ src/Page/SinglePage.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import image from "../../public/singleImg.png";
-import { FaDotCircle, FaDownload, FaRegHeart, FaPlus, FaMinus } from "react-icons/fa";
+import {
+  FaDotCircle,
+  FaDownload,
+  FaRegHeart,
+  FaPlus,
+  FaMinus,
+} from "react-icons/fa";
 
 const SinglePage = () => {
-
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(1);
 
   //incrementQuantity
   const incrementQuantity = (data) => {
-    if(data){
-      setQuantity(data + 1)
+    if (data) {
+      setQuantity(data + 1);
     }
-  }
+  };
   //decrementQuantity
   const decrementQuantity = (data) => {
-    if(data > 0){
-      setQuantity(data - 1)
+    if (data > 0) {
+      setQuantity(data - 1);
     }
-  }
+  };
 
   return (
     <div className="mt-5">
@@ -27,19 +32,34 @@ const SinglePage = () => {
         <div className="sm:flex sm:justify-between gap-8">
           {/* left item section start */}
           <div className="w-full sm:w-[50%]">
-            <img src={image} className="w-full h-44 sm:h-56 md:h-52 lg:h-60 xl:h-80 rounded-xl"></img>
+            <img
+              src={image}
+              className="w-full h-44 sm:h-56 md:h-52 lg:h-60 xl:h-80 rounded-xl"
+            ></img>
             <ul className="flex gap-3 justify-between mt-3">
               <li>
-              <img src={image} className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"></img>
+                <img
+                  src={image}
+                  className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"
+                ></img>
               </li>
               <li>
-              <img src={image} className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"></img>
+                <img
+                  src={image}
+                  className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"
+                ></img>
               </li>
               <li>
-              <img src={image} className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"></img>
+                <img
+                  src={image}
+                  className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"
+                ></img>
               </li>
               <li>
-              <img src={image} className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"></img>
+                <img
+                  src={image}
+                  className="w-14 h-14 sm:w-14 sm:h-20 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-20 rounded-xl"
+                ></img>
               </li>
             </ul>
           </div>
@@ -54,26 +74,28 @@ const SinglePage = () => {
             {/* color button section start */}
             <ul className="flex gap-3">
               <li>
-                <FaDotCircle className="btn text-red-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
+                <FaDotCircle className="text-red-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
               </li>
               <li>
-                <FaDotCircle className="btn text-blue-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
+                <FaDotCircle className="text-blue-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
               </li>
               <li>
-                <FaDotCircle className="btn text-orange-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
+                <FaDotCircle className="text-orange-500 text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
               </li>
               <li>
-                <FaDotCircle className="btn text-black text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
+                <FaDotCircle className="text-black text-base sm:text-lg md:text-base lg:text-lg xl:text-3xl"></FaDotCircle>
               </li>
               <li>
-                <FaDotCircle className="btn text-pink-500 text-base sm:text-lg md:text-base xl:text-3xl"></FaDotCircle>
+                <FaDotCircle className="text-pink-500 text-base sm:text-lg md:text-base xl:text-3xl"></FaDotCircle>
               </li>
             </ul>
             {/* color button section end */}
             {/* price and button section start */}
             <div className="flex justify-between items-center">
               {/* price section start */}
-              <p className="text-base sm:text-lg md:text-base lg:text-xl xl:text-3xl font-semibold">$210.00-$250.00</p>
+              <p className="text-base sm:text-lg md:text-base lg:text-xl xl:text-3xl font-semibold">
+                $210.00-$250.00
+              </p>
               {/* price section end */}
               {/* button section start */}
               <ul className="flex gap-3">
@@ -90,16 +112,20 @@ const SinglePage = () => {
             {/* select (size and color) section start */}
             <div className="flex gap-5 justify-between items-center">
               {/* size section start */}
-              <select name="cars" id="cars" className="w-full bg-slate-200 p-1">
-                <option disabled selected>Size</option>
+              <select className="select select-bordered w-full">
+                <option disabled selected>
+                  Size
+                </option>
                 <option value="volvo">Xl</option>
                 <option value="volvo">2Xl</option>
                 <option value="volvo">3Xl</option>
               </select>
               {/* size section end */}
               {/* color section start */}
-              <select name="cars" id="cars" className="w-full bg-slate-200 p-1">
-                <option disabled selected>Color</option>
+              <select className="select select-bordered w-full">
+                <option disabled selected>
+                  Color
+                </option>
                 <option value="volvo">Red</option>
                 <option value="volvo">Green</option>
                 <option value="volvo">Yellow</option>
@@ -108,20 +134,41 @@ const SinglePage = () => {
             </div>
             {/* select (size and color) section end */}
             {/* select shipping section start */}
-            <select name="cars" id="cars" className="w-full bg-slate-200 p-1">
-                <option disabled selected>Shipping Information</option>
-                <option value="volvo">Cargo</option>
-                <option value="volvo">Ship</option>
-                <option value="volvo">Taller</option>
+            <select className="select select-bordered w-full">
+              <option disabled selected>
+                Shipping Information
+              </option>
+              <option value="volvo">Cargo</option>
+              <option value="volvo">Ship</option>
+              <option value="volvo">Taller</option>
             </select>
             {/* select shipping section end */}
             {/* quantity & cart button section start */}
             <div className="md:flex md:justify-between md:items-center gap-1 lg:gap-2 xl:gap-3">
               <ul className="flex items-center justify-between gap-2 w-full">
-                <li className="text-sm sm:text-base md:text-xs lg:text-sm xl:text-lg">Quantity (pieces)</li>
-                <li className="flex gap-3 md:gap-2 xl:gap-5 items-center border-[1px] border-black px-4 md:px-2 lg:px-3 xl:px-4 py-1 md:py-[6px] rounded-md"><FaMinus className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base" onClick={() => decrementQuantity(quantity)}></FaMinus><span className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base">{quantity}</span><FaPlus className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base" onClick={() => incrementQuantity(quantity)}></FaPlus></li>
+                <li className="text-sm sm:text-base md:text-xs lg:text-sm xl:text-lg">
+                  Quantity (pieces)
+                </li>
+                <li className="flex gap-3 md:gap-2 xl:gap-5 items-center border-[1px] border-black px-4 md:px-2 lg:px-3 xl:px-4 py-1 md:py-[6px] rounded-md">
+                  <FaMinus
+                    className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base"
+                    onClick={() => decrementQuantity(quantity)}
+                  ></FaMinus>
+                  <span className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base">
+                    {quantity}
+                  </span>
+                  <FaPlus
+                    className="text-xs sm:text-sm md:text-xs lg:text-sm xl:text-base"
+                    onClick={() => incrementQuantity(quantity)}
+                  ></FaPlus>
+                </li>
               </ul>
-              <button type="button" className="btn bg-slate-900 w-full p-2 text-white rounded-md mt-3 md:mt-0 text-xs md:text-[9px] lg:text-[13px] xl:text-base">Add to Cart</button>
+              <button
+                type="button"
+                className="bg-slate-900 w-full p-2 text-white rounded-md mt-3 md:mt-0 text-xs md:text-[9px] lg:text-[13px] xl:text-base"
+              >
+                Add to Cart
+              </button>
             </div>
             {/* quantity & cart button section end */}
           </div>
