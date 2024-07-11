656137cde3cba242cc89e632135d8f6c0dd3c398
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-07-11 16:51:10 +0600

    adding accordion section in shopSidebar

--- src/Page/ShopPage.jsx
+++ src/Page/ShopPage.jsx
@@ -1,19 +1,14 @@
 import { useEffect, useState } from "react";
 import ShopCart from "./ShopCart";
 
-
-
 const ShopPage = () => {
-
-  const [shopData, setShopData] =useState([])
-
-  
+  const [shopData, setShopData] = useState([]);
 
   useEffect(() => {
-    fetch('shopCart.json')
-    .then((res) => res.json())
-    .then((data) => setShopData(data))
-  },[])
+    fetch("shopCart.json")
+      .then((res) => res.json())
+      .then((data) => setShopData(data));
+  }, []);
 
   return (
     <div className="mt-5">
@@ -22,13 +17,64 @@ const ShopPage = () => {
         {/* items section start */}
         <div className="md:flex md:justify-between md:gap-5">
           {/* shopSidebar section start */}
-          <div className="hidden md:block md:w-[20%] bg-slate-300">
-            <ul>
-              <li>one</li>
-              <li>two</li>
-              <li>three</li>
-              <li>four</li>
-            </ul>
+          <div className="hidden md:block md:w-[20%]">
+            <div className="border-base-300 border p-5">
+              <details className="collapse collapse-arrow  rounded-md border-base-300">
+                <summary className="collapse-title text-sm bg-slate-100 font-medium ">
+                  Men
+                </summary>
+                <div className="collapse-content py-2">
+                  <ul className="space-y-1">
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">one</span></li>
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">two</span></li>
+                  </ul>
+                </div>
+              </details>
+              <details className="collapse collapse-arrow  rounded-md border-base-300 border">
+                <summary className="collapse-title text-base bg-slate-100 font-medium">
+                  Kinds
+                </summary>
+                <div className="collapse-content py-2">
+                  <ul className="space-y-1">
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">one</span></li>
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">two</span></li>
+                  </ul>
+                </div>
+              </details>
+              <details className="collapse collapse-arrow  rounded-md border-base-300 border">
+                <summary className="collapse-title text-base bg-slate-100 font-medium">
+                  Brands
+                </summary>
+                <div className="collapse-content py-2">
+                  <ul className="space-y-1">
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">one</span></li>
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">two</span></li>
+                  </ul>
+                </div>
+              </details>
+              <details className="collapse collapse-arrow  rounded-md border-base-300 border">
+                <summary className="collapse-title text-base bg-slate-100 font-medium">
+                  Price
+                </summary>
+                <div className="collapse-content py-2">
+                  <ul className="space-y-1">
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">one</span></li>
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">two</span></li>
+                  </ul>
+                </div>
+              </details>
+              <details className="collapse collapse-arrow  rounded-md border-base-300 border">
+                <summary className="collapse-title text-base bg-slate-100 font-medium">
+                  Size
+                </summary>
+                <div className="collapse-content py-2">
+                  <ul className="space-y-1">
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">one</span></li>
+                    <li className="flex items-center gap-2"><input type="checkbox" defaultChecked className="checkbox checkbox-sm" /><span className="text-sm">two</span></li>
+                  </ul>
+                </div>
+              </details>
+            </div>
           </div>
           {/* shopSidebar section end */}
           {/* shopProducts section start */}
@@ -67,7 +113,7 @@ const ShopPage = () => {
               {/* filter data section start */}
               <div className="flex flex-wrap gap-2 w-full">
                 {/* 1 */}
-              <span className="badge gap-2 p-2">
+                <span className="badge gap-2 p-2">
                   <span className="text-xs xl:text-lg">Tops</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +131,7 @@ const ShopPage = () => {
                 </span>
                 {/* 1 */}
                 {/* 1 */}
-              <div className="badge gap-2 p-2">
+                <div className="badge gap-2 p-2">
                   <span className="text-xs xl:text-lg">Tops</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +149,7 @@ const ShopPage = () => {
                 </div>
                 {/* 1 */}
                 {/* 1 */}
-              <div className="badge gap-2 p-2">
+                <div className="badge gap-2 p-2">
                   <span className="text-xs xl:text-lg">Tops</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -126,9 +172,9 @@ const ShopPage = () => {
             {/* filter section end */}
             {/* cart section start */}
             <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-3 sm:gap-3 md:gap-3 lg:gap-3 xl:gap-5 2xl:gap-3">
-              {
-                shopData.map((data, index) => <ShopCart key={index} data={data}></ShopCart>)
-              }
+              {shopData.map((data, index) => (
+                <ShopCart key={index} data={data}></ShopCart>
+              ))}
             </div>
             {/* cart section end */}
           </div>
